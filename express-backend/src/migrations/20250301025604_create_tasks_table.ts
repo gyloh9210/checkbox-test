cccdddeb17f50e43154e6a4be7ebf7f1973f172f
gyloh <ronaldo_loh@hotmail.com>
2025-03-01 11:37:41 +0800

    add tasks table schema

---
+++ express-backend/src/migrations/20250301025604_create_tasks_table.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("tasks", (table) => {
+    table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()")); // UUID primary key
+    table.string("name").notNullable();
+    table.text("description").notNullable();
+    table.date("due_date").notNullable();
+    table.boolean("is_urgent").defaultTo(false);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("tasks");
+}
