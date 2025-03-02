d50d9f3a095d91bc4201d78b72e3e3217a73a616
gyloh <ronaldo_loh@hotmail.com>
2025-03-02 14:35:08 +0800

    add an input for searching task name

--- task-dashboard/src/components/TaskList.tsx
+++ task-dashboard/src/components/TaskList.tsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Paginator, PaginatorPageChangeEvent } from "primereact/paginator";
+import { useDebounce } from '@uidotdev/usehooks';
 import { useGetTasksQuery } from "../api/task";
 import { ITask } from "../types/task";
 import Task from "./Task";
+import { InputText } from "primereact/inputtext";
 
 const TaskList = () => {
   const [keyword, setKeyword] = useState<string>("");
+  const debouncedSearch = useDebounce(keyword, 1000); // prevent overwhelming requests
   const [sortBy, setSortby] = useState<string>("created_at");
   const [order, setOrder] = useState<string>("desc");
   const [page, setPage] = useState<number>(0);
   const { data, isFetched } = useGetTasksQuery({
     page: page + 1,
-    keyword,
+    keyword: debouncedSearch,
     sortBy,
     order,
   });
@@ -20,9 +23,20 @@ const TaskList = () => {
     setPage(e.page);
   };
 
+  const handleKeywordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value)
+  }
+
   return (
     <div>
       <h2 className="text-center p-0 m-0">Task List</h2>
+      <div className="px-3">
+        <InputText
+          value={keyword}
+          onChange={handleKeywordChange}
+          placeholder="Search task name"
+        />
+      </div>
       <div className="overflow-scroll surface-overlay m-3 h-30rem">
         {isFetched ? (
           data?.tasks.map((task: ITask) => <Task key={task.id} data={task} />)
