bd23e86f07d6edf83a63204ffaf589687a063594
gyloh <ronaldo_loh@hotmail.com>
2025-03-02 15:32:07 +0800

    add primeicon package

--- task-dashboard/src/App.tsx
+++ task-dashboard/src/App.tsx
@@ -1,6 +1,7 @@
 import "primereact/resources/themes/lara-light-blue/theme.css"; // Theme
 import "primereact/resources/primereact.min.css"; // Core CSS
 import "primeflex/primeflex.css"; // Flex utilities
+import 'primeicons/primeicons.css';
 import TaskList from "./components/TaskList";
 
 function App() {  
